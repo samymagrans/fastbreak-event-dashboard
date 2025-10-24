ea6e668a5ee559449d04608e645278f1659f1605
Samantha Magrans <samanthamagrans@hotmail.com>
2025-10-23 22:15:16 -0400

    Fix getServerSupabase for Next.js 15 async cookies()

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true,
+    serverActions: {
+      bodySizeLimit: '2mb',   // optional default
+    },
   },
-  output: "standalone",
+  output: 'standalone',
   reactStrictMode: true,
 };
 
